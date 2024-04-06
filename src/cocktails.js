32c681f73426b16f85432b3e6d63a220ddeb2e4a
ben-eh <bencalef@gmail.com>
2024-04-06 18:53:05 -0400

    new cocktails added

--- src/cocktails.js
+++ src/cocktails.js
@@ -18,5 +18,35 @@ export const cocktails = [
 		name: 'Penicillin',
 		photo: 'penicillin.webp',
 		ingredients: ["blended scotch", "fresh ginger", "honey syrup", "lemon juice", "peated scotch"]
+	},
+	{
+		name: 'Amaretto Sour',
+		photo: 'amarettoSour.webp',
+		ingredients: ['amaretto', 'rye/bourbon', 'lemon juice', 'egg white', 'simple syrup']
+	},
+	{
+		name: 'Bourbon Renewal',
+		photo: 'bourbonRenewal.webp',
+		ingredients: ['bourbon', 'creme de cassis', 'lemon juice', 'simple syrup', 'angostura bitters']
+	},
+	{
+		name: 'La Louisiane',
+		photo: 'laLouisiane.webp',
+		ingredients: ['rye', 'sweet vermouth', 'benedictine', 'absinthe', 'peychauds bitters']
+	},
+	{
+		name: 'Manhattan',
+		photo: 'manhattan.webp',
+		ingredients: ['rye/bourbon', 'sweet vermouth', 'angostura bitters']
+	},
+	{
+		name: 'New York Sour',
+		photo: 'newYorkSour.webp',
+		ingredients: ['rye/bourbon', 'lemon juice', 'simple syrup', 'red wine', 'egg white']
+	},
+	{
+		name: 'Rusty Nail',
+		photo: 'rustyNail.webp',
+		ingredients: ['blended scotch', 'drambuie']
 	}
 ]
