4b16d36a11cff5425a4c6b7e6a483ccd2705731e
ben-eh <bencalef@gmail.com>
2024-04-06 16:06:20 -0400

    initiali commit and project almost done, need some styling

---
+++ src/cocktails.js
@@ -0,0 +1,22 @@
+export const cocktails = [
+	{
+		name: 'Daiquiri',
+		photo: 'daiquiri.webp',
+		ingredients: ["white rum", "lime juice", "simple syrup"]
+	},
+	{
+		name: 'Old Fashioned',
+		photo: 'oldFashioned.webp',
+		ingredients: ["rye/bourbon", "simple syrup", "angostura bitters"]
+	},
+	{
+		name: 'Last Word',
+		photo: 'lastWord.webp',
+		ingredients: ["gin", "chartreuse", "lime juice", "maraschino liqueur"]
+	},
+	{
+		name: 'Penicillin',
+		photo: 'penicillin.webp',
+		ingredients: ["blended scotch", "fresh ginger", "honey syrup", "lemon juice", "peated scotch"]
+	}
+]
